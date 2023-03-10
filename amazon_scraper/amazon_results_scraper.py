9012dbd6174535b007724cc1b2b8b6f8e63abe8c
Lennin <lenninssp1021@gmail.com>
2023-03-10 07:59:50 -0500

    Cerca de hacer que funcione el web scrapper

--- amazon_scraper/amazon_results_scraper.py
+++ amazon_scraper/amazon_results_scraper.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys 
+import pandas as pd
 
 # //span[text()='']
 
@@ -24,24 +25,37 @@ def iniciar (item):
     pokemon_reviews = []
 
     # all itlems
-    # name ## //span[@class='a-size-base-plus a-color-base a-text-normal']
-    names = driver.find_elements(By.XPATH, "//span[@class='a-size-base-plus a-color-base a-text-normal']")
-    for name in names:
-        pokemon_names.append(name.text)
-    # price
-    prices = driver.find_elements(By.XPATH, "//span[@class='a-price-whole']")
-    for price in prices:
-        pokemon_prices.append(price.text)
-    #reviews
-    reviews = driver.find_elements(By.XPATH, "//span[@class='a-size-base s-underline-text']")
-    for review in reviews:
-        pokemon_reviews.append(review.text)
+    all_pokemons = driver.find_elements(By.XPATH, ' //div[@data-component-type="s-search-result"]')
+    
+    for pokemon in all_pokemons:
+        # name ## //span[@class='a-size-base-plus a-color-base a-text-normal']
+        names = pokemon.find_elements(By.XPATH, ".//span[@class='a-size-base-plus a-color-base a-text-normal']")
+        for name in names:
+            pokemon_names.append(name.text)
+        # price
+        prices = pokemon.find_elements(By.XPATH, ".//span[@class='a-price-whole']")
+        for price in prices:
+            pokemon_prices.append(price.text)
+        #reviews
+        try:
+            if(pokemon.find_elements(By.XPATH, ".//span[@class='a-size-base s-underline-text']"))>0:
+                reviews = pokemon.find_elements(By.XPATH, ".//span[@class='a-size-base s-underline-text']")
+                for review in reviews:
+                    pokemon_reviews.append(review.text)
+            else:
+                pokemon_reviews.append("0")
+                
+        except:
+            pass 
+
 
 
     print(len(pokemon_names))
     print(len(pokemon_prices))
     print(len(pokemon_reviews))
 
+    df = pd.DataFrame(zip(pokemon_names,pokemon_prices,pokemon_reviews), columns=['pokemon_names','pokemon_prices','pokemon_reviews'])
+    df.to_csv("/Users/lenninsabogal/Coding/Python/jupyters/pokemon/amazon_scraper/pokemon_prices.csv",index=False)
 
 iniciar("pokemon")
 
