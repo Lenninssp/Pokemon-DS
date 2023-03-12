42c2f7f8419418da5420c75dfe7d465a143ae58f
Lennin <lenninssp1021@gmail.com>
2023-03-12 17:05:29 -0500

    creacion de dos ramas nuevas

--- mercado_libre/better_mercado_libre_scrapper.py
+++ mercado_libre/better_mercado_libre_scrapper.py
@@ -38,31 +38,15 @@ def iniciar (item):
     # all itlems
     for _ in range(39): #Cambia 10 por el número de páginas que quieres recorrer
         all_pokemons = driver.find_elements(By.XPATH, "//div[@class='ui-search-result__wrapper shops__result-wrapper']")
-        print(len(all_pokemons))
-        for pokemon in all_pokemons:
-            # name 
-            names = pokemon.find_elements(By.XPATH, ".//h2[@class='ui-search-item__title shops__item-title']")
-            #print(len(names))
-            for name in names:
-                pokemon_names.append(name.text)
-            # price
-            prices = pokemon.find_elements(By.XPATH, ".//span[@class='price-tag-fraction']")
-            #print(len(prices))
-            num=0
-            for m , price in enumerate(prices):
-                pokemon_prices[m].append(price.text)
-                num=m
-            pokemon_prices[num+1].append("0")
-            
-        next_button = driver.find_element(By.XPATH, ".//a[@title='Siguiente']")
-        next_button.click()
 
-                
+        for pokemon in all_pokemons:
+            pokemon.click()
+
     #print(pokemon_names,pokemon_prices[0], pokemon_prices[1],pokemon_prices[2])
     #for a,b in enumerate(pokemon_names):
     #    print(a,b)
     print(pokemon_prices[2])
-    df = pd.DataFrame(zip(pokemon_names,pokemon_prices[0],pokemon_prices[1],pokemon_prices[2]), columns=['pokemon_names','pokemon_prices 1','pokemon_prices 2','pokemon_prices 3'])
+    df = pd.DataFrame(zip(), columns=['pokemon_names','pokemon_branch','pokemon_line','pokemon_model','pokemon_kit(bool)','pokemon_form', 'pokemon_character', 'pokemon_size'])
 
     df = df.reset_index()
     df = df.rename (columns={'index':'contador'})
